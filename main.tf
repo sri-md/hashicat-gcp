d36deac82ba8ec2d3788cf15b506c4bd2b44e588
srimd <srilata.mitra@gmail.com>
2021-05-27 11:59:52 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "tls_private_key" "ssh-key" {
 resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
-  labels {
+  labels = {
   department = "devops"
   }
   machine_type = var.machine_type
