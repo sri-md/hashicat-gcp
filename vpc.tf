7c59c85a9f4bb246d3750ead5228b94bdf61603b
srimd <srilata.mitra@gmail.com>
2021-05-27 12:46:23 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,12 @@
-module "vpc-mod-01" {
-  source       = "../../"
-  project_id   = var.project
-  network_name = gaurav-network
-subnets = [
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
+
+    project_id   = "p-q3bavz9fuaqr0dfrsk2dwfrn43c7"
+    network_name = "gaurav-network"
+    routing_mode = "GLOBAL"
+
+    subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
@@ -10,4 +14,5 @@ subnets = [
   }
 ]
 }
+
 
